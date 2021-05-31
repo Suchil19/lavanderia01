6b970391cd8f6f3714bbadce2684c3debcfcf0a5
Luis Suchil <jlsuchil@gmail.com>
2021-05-31 16:53:55 -0500

    Primer comit

---
+++ recursos/js/card.js
@@ -0,0 +1,24 @@
+/*====== CAROUSEL SERVICIOS =======*/
+$(document).ready(function () {
+    var swiper = new Swiper('.blog-slider', {
+      autoplay: true,
+      spaceBetween: 30,
+      effect: 'fade',
+      loop: true,
+      mousewheel: {
+        invert: false,
+      },
+      // autoHeight: true,
+      pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+      }
+    })});
+    /** TERMINA CAROUSEL SERVICIOS */
+    
+
+  /** SOCIAL BUTTON */
+  // para agergar cualquier clase
+  $(document).ready(function () {
+
+  });
